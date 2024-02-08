009b89d57d6a85207ea88c68d1028debd5f7af59
ralbokhe <159328632+ralbokhe@users.noreply.github.com>
2024-02-08 13:12:36 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,5 @@
-Hello('Hello, World!')
+#This repository Is For Rasheed
+#I am a student at Pima Community College
+Print('This repository is for Rasheed I am a student at Pima Community College')
+print('I like to play soccer in my free time')
+print('In my free time i like to work in my car ')
